cdddbfd49d1a1b1342af11006f5298eb2208859a
20190965YoungJin <dudwls1564954@naver.com>
2024-07-09 23:14:57 +0900

    결과창UI 동기화

--- Assets/00.YJ/YJScript/ResultPageUpdate.cs
+++ Assets/00.YJ/YJScript/ResultPageUpdate.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using System.Linq;
 
 public class ResultPageUpdate : MonoBehaviour
 {
+    public Image babyUI;
+    public Sprite happyBaby;
+    public Sprite cryBaby;
+
     public TextMeshProUGUI failTextUI;
+    public Image bronzeMedal;
+    public Image sliverMedal;
+    public Image goldMedal;
+
+    private FirstPersonCam firstPersonCam;
+
+    private void Awake()
+    {
+        firstPersonCam = FindAnyObjectByType<FirstPersonCam>();
+    }
 
     private void OnEnable()
     {
-        // MissionManager 인스턴스의 미션 리스트를 가져옴
+        firstPersonCam.enabled = false;
+        
         var missions = MissionManager.Instance.missions;
 
         // isCompleted가 false인 미션 중 가장 마지막 미션을 찾음
@@ -16,13 +32,44 @@ public class ResultPageUpdate : MonoBehaviour
 
         if (lastUncompletedMission != null)
         {
-            // failTextUI에 missionFailText를 업데이트
+            Debug.Log($"{lastUncompletedMission.missionFailText}");
             failTextUI.text = lastUncompletedMission.missionFailText;
         }
         else
         {
-            // 완료되지 않은 미션이 없는 경우 기본 텍스트 설정
-            failTextUI.text = "모든 미션이 완료되었습니다.";
+            failTextUI.text = "아이가 편안한 모습으로 자고 있습니다.";
         }
+
+        // 완료된 미션 개수
+        int uncompletedMissionCount = missions.Count(m => m.isCompleted);
+        Debug.Log($"{uncompletedMissionCount}");
+
+        if(uncompletedMissionCount > 2)
+        {
+            bronzeMedal.color = Color.white;
+            babyUI.sprite = cryBaby;
+            if (uncompletedMissionCount > 5)
+            {
+                sliverMedal.color = Color.white;
+                babyUI.sprite = cryBaby;
+                if (uncompletedMissionCount > 8)
+                {
+                    goldMedal.color = Color.white;
+                    babyUI.sprite = happyBaby;
+                }  
+            }
+        }
+        else
+        {
+            bronzeMedal.color = Color.black;
+            sliverMedal.color = Color.black;
+            goldMedal.color = Color.black;
+            babyUI.sprite = cryBaby;
+        }
+    }
+
+    private void OnDisable()
+    {
+        firstPersonCam.enabled = true;
     }
 }
