c3fdcf97404785e9eb9cacca7b1208af26c8c489
이정민 <79199117+daniellee1019@users.noreply.github.com>
2024-07-09 21:49:37 +0900

    아기 agent

--- Assets/02.Scripts/Agent/BabyAgent.cs
+++ Assets/02.Scripts/Agent/BabyAgent.cs
@@ -8,8 +8,10 @@ public class BabyAgent : MonoBehaviour
 
     public float wanderRadius = 10f; // 아기가 이동할 범위 반경
     public float wanderTimer = 5f; // 새로운 목적지를 설정하는 시간 간격
+    public Transform holdPos;
 
     private float timer;
+    private bool isInCrib = false; // 아기가 crib 안에 있는지 여부
 
     void Start()
     {
@@ -21,14 +23,23 @@ public class BabyAgent : MonoBehaviour
 
     void Update()
     {
-        timer += Time.deltaTime;
-
-        if (timer >= wanderTimer)
+        if (!isInCrib)
         {
-            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
-            this._agent.SetDestination(newPos);
-            SetAnimationState("Crawl");
-            timer = 0;
+            timer += Time.deltaTime;
+
+            if (timer >= wanderTimer)
+            {
+                Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+                _agent.SetDestination(newPos);
+                SetAnimationState("Crawl");
+                timer = 0;
+            }
+        }
+        else
+        {
+            // 아기가 crib 안에 있는 동안 목적지 취소 및 "Sleep" 애니메이션 유지
+            _agent.SetDestination(holdPos.position);
+            SetAnimationState("Sleep");
         }
     }
 
@@ -58,5 +69,32 @@ public class BabyAgent : MonoBehaviour
     private void SetLayerPriority(int StateLayer = 1, int Priority = 1) // 애니메이터의 레이어 우선순위 값(무게) 설정
     {
         _anim.SetLayerWeight(StateLayer, Priority);
+    }
+
+    // 콜라이더에 들어왔을 때 처리하는 메소드
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("babyCrib"))
+        {
+            isInCrib = true;
+        }
+    }
+
+    // 콜라이더에 있는 동안 처리하는 메소드
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("babyCrib"))
+        {
+            isInCrib = true;
+        }
+    }
+
+    // 콜라이더에서 나갔을 때 처리하는 메소드
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("babyCrib"))
+        {
+            isInCrib = false;
+        }
     }
 }
