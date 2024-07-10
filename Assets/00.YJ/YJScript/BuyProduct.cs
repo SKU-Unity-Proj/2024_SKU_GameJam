41c389198d3711ea8a36943ce2ddd6a27676886f
20190965YoungJin <dudwls1564954@naver.com>
2024-07-10 18:42:05 +0900

    0

--- Assets/00.YJ/YJScript/BuyProduct.cs
+++ Assets/00.YJ/YJScript/BuyProduct.cs
@@ -1,34 +1,71 @@
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class BuyProduct : MonoBehaviour
 {
-    public GameObject selectProduct;
-    private float distanceFromCamera = 1.0f;
+    public int buyId;
 
     public GameObject carpet;
     public GameObject corner;
     public GameObject jokjoke;
 
+    public TextMeshProUGUI numberText; // UI 텍스트 컴포넌트
+
+    private void Awake()
+    {
+        if(jokjoke != null)
+            carpet.SetActive(false); corner.SetActive(false); jokjoke.SetActive(false);
+    }
+
     public void BuyProductBtn()
     {
-        Vector3 cameraPosition = Camera.main.transform.position;
-        Vector3 cameraForward = Camera.main.transform.forward;
+        if(buyId != 0)
+        {
+            int price = 0;
+            if(buyId == 1)
+            {
+                price = 70000;
+                carpet.SetActive(true);
+                SubtractFromNumber(price);
+                return;
+            }
 
-        // 카메라에서 지정된 거리만큼 떨어진 위치를 계산합니다.
-        Vector3 spawnPosition = cameraPosition + cameraForward * distanceFromCamera;
+            if (buyId == 2)
+            {
+                price = 20000;
+                corner.SetActive(true);
+                SubtractFromNumber(price);
+                return;
+            }
 
-        // 오브젝트를 생성합니다.
-        if (selectProduct != null)
-            Instantiate(selectProduct, spawnPosition, Quaternion.identity);
+            if (buyId == 3)
+            {
+                price = 8000;
+                jokjoke.SetActive(true);
+                SubtractFromNumber(price);
+                return;
+            }
+        }
     }
 
     public void ChangeSelectProduct(int i)
     {
-        if (i == 1)
-            selectProduct = carpet;
-        if (i == 2)
-            selectProduct = corner;
-        if (i == 3)
-            selectProduct = jokjoke;
+        buyId = i;
+    }
+
+    public void AddToNumber(int amount)
+    {
+        int currentNumber = int.Parse(numberText.text);
+        currentNumber += amount;
+        numberText.text = currentNumber.ToString();
+    }
+
+    // 텍스트에 적힌 숫자에서 빼기
+    public void SubtractFromNumber(int amount)
+    {
+        int currentNumber = int.Parse(numberText.text);
+        currentNumber -= amount;
+        numberText.text = currentNumber.ToString();
     }
 }
